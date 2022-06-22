eacbde1c3c02b69b42da3c80879ec707d0ecad5c
bKfe-j <rsef2id@icloud.com>
2022-06-22 16:54:34 +0900

    0622 DB 매니저 프로젝트 완성.

--- study/src/com/JDBCPrj/DBManager.java
+++ study/src/com/JDBCPrj/DBManager.java
@@ -4,95 +4,154 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 public class DBManager {
+    Scanner scan = new Scanner(System.in);
     private Connection con;
     private PreparedStatement pstat;
     private ResultSet rs;
 
+    // DB와 연결하는 메소드
     public Connection getConnection(View view) {
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             con = DriverManager.getConnection(view.getUrl(), view.getId(), view.getPW());
+            System.out.println("---------------------------");
             System.out.println("연결되었습니다");
+            System.out.println("---------------------------");
+            System.out.print(System.lineSeparator());
             return con;
         } catch (Exception e) {
-            System.out.print("DBConnection() : " + e.getMessage());
+            System.out.println("Error : " + e.getMessage());
         }
         return null;
     }
-    //update: 조회문(select, show 등)을 제외한 create, drop, insert, delete, update 등등 문을 처리할 때 사용한다.
-//Query:  조회문(select, show 등)을 실행할 목적으로 사용한다.
+
+    // select 시퀄문을 실행하는 메소드
     public void selectQuery(View view) {
-        String sql = "select " + view.select + " from " + view.from;
+//        String sql = "select " + view.select + " from " + view.from;
         try {
-            pstat = con.prepareStatement(sql);
-            if (!(view.where == null)) {
-                sql += "" + " WHERE " + view.where;
-            } else if (!(view.groupBy == null)) {
-                sql += "" + " GROUP BY " + view.groupBy;
-            } else if (!(view.having == null)) {
-                sql += "" + " HAVING " + view.having;
-            } else if (!(view.orderBy == null)) {
-                sql += "" + " ORDER BY " + view.orderBy;
-            }
-            pstat = con.prepareStatement(sql);
+//            if (!(view.where == null)) {
+//                sql += "" + " WHERE " + view.where;
+//            } else if (!(view.groupBy == null)) {
+//                sql += "" + " GROUP BY " + view.groupBy;
+//            } else if (!(view.having == null)) {
+//                sql += "" + " HAVING " + view.having;
+//            } else if (!(view.orderBy == null)) {
+//                sql += "" + " ORDER BY " + view.orderBy;
+//            }
+//            String num1=scan.nextLine();
+//            String num2=scan.nextLine();
+
+//            pstat = con.prepareStatement(sql);
+//            pstat.setString(1,num1);
+//            pstat.setString(2,num2);
+            pstat = con.prepareStatement(view.selectSQL);
+
             rs = pstat.executeQuery();
+            System.out.println("---------------------------");
             System.out.println("테이블을 조회했습니다");
+            System.out.println("---------------------------");
         } catch (Exception e) {
-            System.out.println("DB연결 실패하거나, SQL문이 틀렸습니다.");
-            System.out.print("Error : " + e.getMessage());
+            System.out.println("---------------------------");
+            System.out.println("SQL문이 틀렸습니다.");
+            System.out.println("---------------------------");
+            System.out.println("Error : " + e.getMessage());
+        }
+    }
+    public void createQuery(View view){
+//        String sql = "CREATE TABLE USERINFO ( NAME VARCHAR2(10), ID VARCHAR2(10) PRIMARY KEY, PW VARCHAR2(10))";
+        try {
+            pstat = con.prepareStatement(view.createSQL);
+            rs = pstat.executeQuery();
+            System.out.println("---------------------------");
+            System.out.println("테이블이 생성되었습니다.");
+            System.out.println("---------------------------");
+        } catch (Exception e) {
+            System.out.println("---------------------------");
+            System.out.println("SQL문이 틀렸습니다.");
+            System.out.println("---------------------------");
+            System.out.println("Error : " + e.getMessage());
         }
     }
 
+    // 테이블 드랍 시퀄문 메소드.
+    public void dropQuery(View view){
+//        String sql = "DROP TABLE USERINFO";
+        try {
+            pstat = con.prepareStatement(view.dropSQL);
+            rs = pstat.executeQuery();
+            System.out.println("---------------------------");
+            System.out.println("테이블이 삭제되었습니다.");
+            System.out.println("---------------------------");
+        } catch (Exception e) {
+            System.out.println("---------------------------");
+            System.out.println("SQL문이 틀렸습니다.");
+            System.out.println("---------------------------");
+            System.out.println("Error : " + e.getMessage());
+        }
+    }
 
+    // 데이터 값을 입력하는 메소드
+    public void insertQuery(View view){
+//        String sql = "INSERT INTO USERINFO VALUES ('BOSS', 'boss', 'boss')";
+        try {
+            pstat = con.prepareStatement(view.insertSQL);
+            rs = pstat.executeQuery();
+            System.out.println("---------------------------");
+            System.out.println("sql문이 실행되었습니다.");
+            System.out.println("---------------------------");
+        } catch (Exception e) {
+            System.out.println("---------------------------");
+            System.out.println("SQL문이 틀렸습니다.");
+            System.out.println("---------------------------");
+            System.out.println("Error : " + e.getMessage());
+        }
+    }
+
+    // 커스텀 시퀄문을 입력하는 메소드
+    public void customQuery(View view){
+        try {
+            pstat = con.prepareStatement(view.customSQL);
+            rs = pstat.executeQuery();
+            System.out.println("---------------------------");
+            System.out.println("sql문이 실행되었습니다.");
+            System.out.println("---------------------------");
+        } catch (Exception e) {
+            System.out.println("---------------------------");
+            System.out.println("SQL문이 틀렸습니다.");
+            System.out.println("---------------------------");
+            System.out.println("Error : " + e.getMessage());
+        }
+    }
+
+    // DB연결 끝고 Manager 종료 메소드
     public void close() {
         try {
-            con.close();
-            pstat.close();
             rs.close();
+            pstat.close();
+            con.close();
             System.out.println("정상적으로 종료되었습니다.");
         } catch (Exception e) {
-            System.out.print("Error : " + e.getMessage());
+            System.out.println("Error : " + e.getMessage());
         }
     }
 
+    // select된 결과값을 출력하는 메소드. colNum은 출력할 컬럼 갯수.
     public void print(int colNum) {
         try {
+            System.out.println("---------------------------");
             while (rs.next()) {
                 for (int i = 0; i < colNum; i++) {
-                    System.out.print(rs.getString(i + 1) + "\t");
+                    System.out.print(rs.getString(i+1) + "\t");
                 }
                 System.out.print(System.lineSeparator());
             }
+            System.out.println("---------------------------");
+            System.out.print(System.lineSeparator());
         } catch (Exception e) {
-            System.out.print("Error : " + e.getMessage());
+            System.out.println("Error : " + e.getMessage());
         }
     }
-
-//    public void sales(int soldIndex, int amountIndex) {
-//        try {
-//            int sold = rs.getInt(soldIndex);
-//            int amount = rs.getInt(amountIndex);
-//            int totalSales = sold * amount;
-//        } catch (Exception e) {
-//            System.out.print("Error : " + e.getMessage());
-//        }
-//    }
-//
-//    public void insert(String insertInto, String select, String from, String where, String orderBy) {
-//        try {
-//            String sql = "INSERT INTO " + insertInto + " SELECT " + select + " FROM " + from;
-//            if (!(where == null)) {
-//                sql += " " + "WHERE " + where;
-//            } else if (!(orderBy == null)) {
-//                sql += " " + "ORDER BY " + orderBy;
-//            }
-//            pstat = con.prepareStatement(sql);
-//            rs = pstat.executeQuery();
-//        } catch (Exception e) {
-//            System.out.println("DB연결 실패하거나, SQL문이 틀렸습니다.");
-//            System.out.print("Error : " + e.getMessage());
-//        }
-//    }
 }
--- study/src/com/JDBCPrj/Example.java
+++ study/src/com/JDBCPrj/Example.java
@@ -2,19 +2,8 @@ package com.JDBCPrj;
 
 public class Example {
     public static void main(String[] args) {
-        DBManager dbm = new DBManager();
-        View view = new View();
+        Selection sl = new Selection();
 
-        view.setDBInfo("jdbc:oracle:thin:@localhost:1521:XE", "C##TEST","oracle");
-//        view.selectSqlInput();
-        view.tempSql();
-
-        dbm.getConnection(view);
-        dbm.selectQuery(view);
-        dbm.print(1);
-
-        //rs 이용 메소드 위치장소
-
-        dbm.close();
+        sl.start();
     }
 }
